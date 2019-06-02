9dd8405e1d33545aa75452e20ada71ea04de5e6b
mondaugen <nicholas.esterer@gmail.com>
2019-06-02 09:30:22 -0400

    Smoothing the frequency makes it sound nicer
    It's still hard to make the tracker adequately adaptive but also not have it
    just converge to the fundamental. We will now try to put some guards in place so
    that the pitch cannot deviate too much from the initial guess.

--- test/ptrack_one_note_guitar.py
+++ test/ptrack_one_note_guitar.py
@@ -3,12 +3,13 @@ import ptracker
 import numpy as np
 import matplotlib.pyplot as plt
 import librosa
+from scipy import signal
 
 sr=16000
 x=np.fromfile('/tmp/one_guitar_note.f64',dtype='float64')
 t=np.arange(len(x))/sr
 w0=65.406/sr*2*np.pi # frequency of c2
-n_parts=16
+n_parts=32
 
 parts=ptracker.multi_ptrackers(
 x,
@@ -21,12 +22,30 @@ ptrack_args=(
 1e-4)
 )
 
+alph_w=0.999
+alph_a=0.99
+
 t=np.arange(len(x))/sr
 
 fig,axs=plt.subplots(n_parts,2)
+y=np.zeros(t.shape,dtype='float64')
 for n,(a,phi,w) in enumerate(parts):
     axs[n,0].plot(t,w)
     axs[n,1].plot(t,a)
+    w_mean=np.mean(w)
+    w_=w-w_mean
+    w_=signal.lfilter([1-alph_w],[1,-alph_w],w_)
+    a_=signal.lfilter([1-alph_a],[1,-alph_a],a)
+    w_+=w_mean
+    #w_=w
+    a_=a
+    phi_=np.cumsum(w_)
+    y+=a_*np.cos(phi_)
+
+y-=np.mean(y)
+y/=np.max(np.abs(y))
+y*=0.9
+y.tofile('/tmp/synth_guit_nparts=%d.f64' % (n_parts,))
 
 plt.show()
     
