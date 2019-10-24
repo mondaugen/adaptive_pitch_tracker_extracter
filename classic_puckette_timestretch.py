98244a0083a21cf9b888cad75e55eaf9ff09f170
mondaugen <nicholas.esterer@gmail.com>
2019-10-24 07:38:46 -0400

    We correctly compute the window for dividing out
    the analysis and synthesis windows for the python prototype.
    
    Now to change this for the C implementation.

--- classic_puckette_timestretch.py
+++ classic_puckette_timestretch.py
@@ -7,7 +7,10 @@ def time_stretch_arb_times(
     t, # times at which to place windows
     H, # hop size
     W, # window size
-    window_type='hann'):
+    window_type='hann',
+    synth_window_type=None):
+    if synth_window_type is None:
+        synth_window_type=window_type
 
     # check to see that all the times are valid
     # a valid time t is H <= t < len(x)
@@ -20,31 +23,37 @@ def time_stretch_arb_times(
     #x=np.concatenate((np.zeros(H,dtype=x.dtype),x))
     x=np.concatenate((np.random.standard_normal(H).astype(x.dtype)*1e-8,x))
     t_shift=H 
+    # get analysis window
     w=signal.get_window(window_type,W)
+    # get synthesis window
+    sw=signal.get_window(synth_window_type,W)
     y_len=H*(len(t)-1)+W
     y=np.zeros(y_len)
     t0=t[0]+t_shift
     win_1=np.zeros_like(w)
-    s=np.zeros_like(y)
+    # calculate dividing out window, even for hops that aren't multiple of window size
+    w_=np.concatenate((w,np.zeros(H-(W%H))))
+    sw_=np.concatenate((sw,np.zeros(H-(W%H))))
+    #win_div=np.sum(np.power(w,2).reshape((W//H,H)),axis=0)
+    win_div=np.sum((w_*sw_).reshape((len(w_)//H,H)),axis=0)
+    print('win_div:')
+    print(win_div)
     # TODO: Why is the beginning so difficult?
     for h in np.arange(0,W,H):
         #win_1[:W-h]+=w[h:]
         _Y=np.fft.fft(np.concatenate((np.zeros(h),x[t0:t0+W-h]*w[h:])))
         if h == 0:
             Y_last=_Y.copy()
-        y[:W-h]+=np.real(np.fft.ifft(Y_last))[h:]*w[h:]
-        s[:W-h]+=np.power(w[h:],2)
-
+        y[:W-h]+=np.real(np.fft.ifft(Y_last))[h:]*sw[h:]
+    y[:H]/=win_div
     h=H
     for t_ in t[1:]:
         t_=t_+t_shift
         X0=np.fft.fft(x[t_:t_+W]*w)
         X_H=np.fft.fft(x[t_-H:t_-H+W]*w)
         Y_last=X0*np.abs(X_H)/X_H*Y_last/np.abs(Y_last)
-        y[h:h+W]+=np.real(np.fft.ifft(Y_last))*w
-        s[h:h+W]+=np.power(w,2)
+        y[h:h+W]+=np.real(np.fft.ifft(Y_last))*sw
+        y[h:h+H]/=win_div
         h+=H
-    y/=s
 
     return y
-        
--- test/determine_pvoc_synth_window.py
+++ test/determine_pvoc_synth_window.py
@@ -2,12 +2,15 @@ import classic_puckette_timestretch as cpt
 import numpy as np
 
 
-H=8
+H=5
 W=32
 x=np.random.standard_normal(1000)
 a_times=np.arange(0,len(x)-W,H)
+print('hop times')
 print(a_times)
 
-y=cpt.time_stretch_arb_times(x,a_times,H,W,window_type='parzen')
+y=cpt.time_stretch_arb_times(x,a_times,H,W,
+window_type='blackmanharris',
+synth_window_type='hann')
 
 print(y/x[:len(y)])
